10bc1f66fc6b1a57a114295d8056e7611857cbf8
HARSHIT PANDEY <harshitpandey8194@gmail.com>
2026-08-27 00:32:34 +0530

    chore: clean up gitignore and untrack build artifacts

--- .vercel/output/functions/__server.func/_libs/_.mjs
+++
@@ -1,2 +0,0 @@
-import { w as supabase_exports } from "../_ssr/router-dwDVHqyE.mjs";
-export { supabase_exports as n };
--- .vercel/output/static/assets/chevron-right-BNspCDaU.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./index-BUuWkYhg.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
